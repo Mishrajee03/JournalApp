67b997d0e7bf992f6109211977df70e580555905
Mishrajee03 <mishrjeein2006@gmail.com>
2026-01-14 13:15:13 +0530

    first commit

---
+++ src/test/java/com/Mishrajee/journalApp/Service/UserServiceTests.java
@@ -0,0 +1,22 @@
+package com.Mishrajee.journalApp.Service;
+
+
+import com.Mishrajee.journalApp.Repository.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Test
+    public void testFindByUserName(){
+        assertNotNull(userRepo.findByUserName("ram"));
+    }
+}
