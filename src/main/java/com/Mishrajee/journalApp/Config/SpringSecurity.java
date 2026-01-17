b414406ab530c27dfd6a8137bc088783b07d7ca6
Mishrajee03 <mishrjeein2006@gmail.com>
2026-01-17 11:59:35 +0530

    swagger added

--- src/main/java/com/Mishrajee/journalApp/Config/SpringSecurity.java
+++ src/main/java/com/Mishrajee/journalApp/Config/SpringSecurity.java
@@ -38,7 +38,9 @@ public class SpringSecurity {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http.authorizeHttpRequests(request -> request
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/public/**","/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html").permitAll()
                         .requestMatchers("/journal/**", "/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
